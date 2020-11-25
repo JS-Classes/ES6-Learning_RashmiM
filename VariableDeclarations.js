0c7e9803891bf0e1d581d49a821f1f10ceb5c36f
rashmi maharjan <rashmi.maharjan@cotiviti.com>
2020-11-25 13:01:55 +0545

    variable declaration

---
+++ VariableDeclarations.js
@@ -0,0 +1,19 @@
+//Datatypes
+/* for multilines*/
+//Dynamic language
+
+//Primary
+var x = "Nepal";
+var y = true;
+var z = 2345;
+var a = 2n ** 200n; //bigint
+var b = undefined;
+var c = null;
+
+//Structural
+var d = [1,2,3,4];
+var e = {name: "Rashmi", isMale: true};
+
+
+console.log(c);
+
