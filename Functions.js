1e0790d957d1dd6a3c36094084f55be18aa43d65
rashmi maharjan <rashmi.maharjan@cotiviti.com>
2020-11-26 09:59:57 +0545

    Learning functions

---
+++ Functions.js
@@ -0,0 +1,22 @@
+//function addTwoNumbers //camel case
+//function AddTwoNumbers //pascal case
+
+
+function square(number) 
+{
+    let squared = number * number;
+    return squared;
+}
+let s = square(5);
+console.log(s);
+
+//Function expression: anonymous function
+var func1 = function (number) 
+{
+    let squared = number * number;
+    return squared;
+}
+let s1 = func1(6);
+console.log(s1);
+
+//Arrow function
