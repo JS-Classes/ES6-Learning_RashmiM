e29306187f70aba44d2bb284b07c60603d0766c8
rashmi maharjan <rashmi.maharjan@cotiviti.com>
2020-11-26 09:19:29 +0545

    variables2ndclass

--- VariableDeclarations.js
+++ VariableDeclarations.js
@@ -2,18 +2,32 @@
 /* for multilines*/
 //Dynamic language
 
-//Primary
+//Primitive
 var x = "Nepal";
 var y = true;
 var z = 2345;
 var a = 2n ** 200n; //bigint
 var b = undefined;
 var c = null;
+var c1 = +Infinity;
+var c2 = -Infinity;
+var c3 = NaN; //not a number
+var x = "dfsfs"/34;
+// console.log(x);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+var d = 3n ** 123n;
+var e = undefined;
+var f = null;
+
 
 //Structural
-var d = [1,2,3,4];
-var e = {name: "Rashmi", isMale: true};
+var d = ["a","b"];
+var e = {name: "Rashmi", age:12};
 
 
-console.log(c);
+console.log(typeof b);
+console.log(typeof a);
+console.log(typeof e);
+console.log(typeof f);
 
