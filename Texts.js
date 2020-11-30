4ecb06f38133108c61b017637f4eafcb79396a8a
rashmi maharjan <rashmi.maharjan@cotiviti.com>
2020-11-30 12:58:26 +0545

    Learn Strings

---
+++ Texts.js
@@ -0,0 +1,16 @@
+
+let hackathon ='Hackathon 2020' + 12;
+let x = "X";
+let y = "Y";
+let isMale = true;
+
+// String literal
+let name = `Cotiviti ${12+34-56}`;
+// console.log(name);
+
+let a ="12 432 4234 gdf 32 4j gfd sg732jgdjs";
+let re = /gd/;
+// let re1 = new RegExp('[0-9]');
+
+let b = a.match(re);
+console.log(b);
